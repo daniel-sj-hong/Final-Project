20e09ee90fd3809e0165135c2728ce44b788faff
Daniel Hong <dan8492@gmail.com>
2021-09-16 17:21:45 -0700

    installed the date-fns package to format the date of a review to the way i wanted it to and managed to get it formatted correctly

--- client/pages/details.jsx
+++ client/pages/details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
 import ReactStars from 'react-rating-stars-component';
+import { format } from 'date-fns';
 
 export default class Details extends React.Component {
   constructor(props) {
@@ -30,6 +31,11 @@ export default class Details extends React.Component {
 
   render() {
     if (this.state.isLoading) return null;
+    const timestamp = this.state.reviews[0].time_created;
+    const date = new Date(timestamp);
+    const dateFormated = format(date, 'MM/dd/yyyy');
+    console.log(dateFormated);
+    console.log(this.state.reviews[0].time_created);
     return (
       <>
         <Header />
@@ -62,10 +68,10 @@ export default class Details extends React.Component {
                 </div>
               </div>
               <div className="row">
-                <div className="col-one-thirds flex center-all font-bold">
+                <div className="col-one-thirds flex center-all font-bold font-adjust">
                   {this.state.searchResults.price}
                 </div>
-                <div className="col-one-thirds flex center-all font-bold">
+                <div className="col-one-thirds flex center-all font-bold font-adjust-2">
                   {this.state.searchResults.display_phone}
                 </div>
                 <div className="col-one-thirds flex center-all">
@@ -73,6 +79,28 @@ export default class Details extends React.Component {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="row">
+            <div className="col-full">
+              <h2 className="margin-left-15">Reviews</h2>
+            </div>
+          </div>
+          <div className="row">
+            <ul className="row justify-center">
+              <li className="col-90">
+                <div className="row">
+                  <div className="col-one-thirds flex center-all">
+                    <p>{this.state.reviews[0].user.name}</p>
+                  </div>
+                  <div className="col-one-thirds flex center-all">
+                    <ReactStars size={15} value={this.state.reviews[0].rating} edit={false} isHalf={true} />
+                  </div>
+                  <div className="col-one-thirds flex center-all">
+                    <p>{dateFormated}</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </>
