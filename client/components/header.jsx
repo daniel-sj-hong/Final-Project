8fe408c800a1f7e8ca1cf9c03fd7a9c12b20a06b
Daniel Hong <dan8492@gmail.com>
2021-09-18 14:07:07 -0700

    got my app drawer to work and handles changing pages

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-export default function Header(props) {
-  return (
-    <>
-      <nav className="header-color">
-        <p className="header align-center">Choice.ly</p>
-      </nav>
-    </>
-  );
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOn: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => {
+      return { isModalOn: !prevState.isModalOn };
+    });
+  }
+
+  render() {
+
+    return (
+      <>
+        <header className="header-color flex justify-center">
+          <p className="header">Choice.ly</p>
+          <i onClick={this.toggle} className="fas fa-bars app-icon"></i>
+        </header>
+          {this.state.isModalOn &&
+            <>
+              <div onClick={this.toggle} className={'modal-background absolute'}></div>
+              <div className={'app-drawer absolute'}>
+                <h1 className="margin-top-0 margin-left-20">Menu</h1>
+                <ul className="padding-0">
+                  <li className="list-item" onClick={this.toggle}><a className="padding-10" href="#favorites">Favorites</a></li>
+                  <li className="list-item" onClick={this.toggle}><a className="padding-10" href="#">Back to categories</a></li>
+                </ul>
+              </div>
+            </>
+          }
+      </>
+    );
+
+  }
+
 }
